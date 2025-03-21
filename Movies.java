2abe86fd03e224d13ec67353a023c0dd00630c30
Tejaswa <tejaswad18@gmail.com>
2025-03-21 11:45:47 +0530

    Arrays Tasks Are pushed

---
+++ Movies.java
@@ -0,0 +1,24 @@
+class Movies{
+	public static void main(String args[]){
+		String hindiSeries[]={"Sacred Games","Mirzapur","Scam 1992: The Harshad Mehta Story","The Family Man","Aspirants","Criminal Justice"," Breathe","Panchayat","Paatal Lok","Special OPS"};
+		System.out.println("The total number of Hindi series available is "+hindiSeries.length);
+		System.out.println(hindiSeries[0]+" \n"+hindiSeries[1]+" \n"+hindiSeries[2]+" \n"+hindiSeries[3]+" \n"+hindiSeries[4]+" \n"+hindiSeries[5]+" \n"+hindiSeries[6]+" \n"+hindiSeries[7]+" \n"+hindiSeries[8]+" \n"+hindiSeries[9]);
+		System.out.println("");
+		
+		String englishSeries[]={"Game of Thrones","Breaking Bad","Stranger Things","Friends","The Walking Dead","Sherlock"," The Big Bang Theory","Dexter","The Office","The Boys"};
+		System.out.println("The total number of english series available is "+englishSeries.length);
+		System.out.println(englishSeries[0]+" \n"+englishSeries[1]+" \n"+englishSeries[2]+" \n"+englishSeries[3]+" \n"+englishSeries[4]+" \n"+englishSeries[5]+" \n"+englishSeries[6]+" \n"+englishSeries[7]+" \n"+englishSeries[8]+" \n"+englishSeries[9]);
+		System.out.println("");
+		
+		String kannadaSeries[]={"Suzhal - The Vortex","Unlock- The Haunted App","Love Under Construction","The 'Other' Love Story","The Hunt for Veerappan","Crime Stories: India Detectives"," Nandhini","Bigg Boss Kannada"," Honeymoon","Game"};
+		System.out.println("The total number of kannada series available is "+kannadaSeries.length);
+		System.out.println(kannadaSeries[0]+" \n"+kannadaSeries[1]+" \n"+kannadaSeries[2]+" \n"+kannadaSeries[3]+" \n"+kannadaSeries[4]+" \n"+kannadaSeries[5]+" \n"+kannadaSeries[6]+" \n"+kannadaSeries[7]+" \n"+kannadaSeries[8]+" \n"+kannadaSeries[9]);
+		System.out.println("");
+		
+		String malyalamSeries[]={"Suzhal - The Vortex","Unmadham","1000 Babies","Kaalkoot"," Poacher","Perilloor Premier League"," Soul Stories","Love Under Construction","Selinte Tuition Class","Idiyappam"};
+		System.out.println("The total number of malyalam series available is "+malyalamSeries.length);
+		System.out.println(malyalamSeries[0]+" \n"+malyalamSeries[1]+" \n"+malyalamSeries[2]+" \n"+malyalamSeries[3]+" \n"+malyalamSeries[4]+" \n"+malyalamSeries[5]+" \n"+malyalamSeries[6]+" \n"+malyalamSeries[7]+" \n"+malyalamSeries[8]+" \n"+malyalamSeries[9]);
+		
+		
+	}
+}
