a9a7c6576189f634ef46db9653c627ec9a0cca15
Tejaswa <tejaswad18@gmail.com>
2025-03-21 11:17:31 +0530

    Objects and Classes are Pushed

--- PinCode.java
+++
@@ -1,9 +0,0 @@
-class PinCode{
-	public static void main(String args[]){
-		int pinCodes[]={584128,577102,577101,500003};
-		System.out.println("The pincodes are: ");
-		for (int pinCode : pinCodes){
-			System.out.println(pinCode);	
-		}
-	}
-}
